1f1812670febeb7b5871da7f7f7826b7dec78c4d
james-mathe <kambalemathejacques3o@gmail.com>
2025-04-01 23:23:06 +0200

    Update for Interfaces

--- interfacesAndAbstactClasses/interfacesAndAbstactClasses/Program.cs
+++ interfacesAndAbstactClasses/interfacesAndAbstactClasses/Program.cs
@@ -66,24 +66,24 @@ namespace interfacesAndAbstactClasses
 
                     if (choix_class == 2)
                     {
-                        Interface.Motorcycle m1 = new Interface.Motorcycle("UJ898", "HAUJIN", "Red", "4537788egte");
-                        Interface.Cars c1 = new Interface.Cars("fgt34", "WOLVSWAGGEN", "Metal", "oo9ieiie");
-                        Interface.Motorcycle m2 = new Interface.Motorcycle("UJ898", "HAUJIN", "Red", "4537788egte");
-                        Interface.Cars c2 = new Interface.Cars("fgt34", "WOLVSWAGGEN", "Metal", "oo9ieiie");
-                        Interface.Motorcycle m3 = new Interface.Motorcycle("UJ898", "HAUJIN", "Red", "4537788egte");
-                        Interface.Cars c3 = new Interface.Cars("fgt34", "WOLVSWAGGEN", "Metal", "oo9ieiie");
-                        m1.Add(choix_db);
-                        c1.Add(choix_db);
-                        m2.Add(choix_db);
-                        c2.Add(choix_db);
-                        m3.Add(choix_db);
-                        c3.Add(choix_db);
-                        m1.showDetails(choix_db, 1);
-                        c1.showDetails(choix_db, 1);
-                        m2.showDetails(choix_db, 2);
-                        c2.showDetails(choix_db, 2);
-                        m3.showDetails(choix_db, 3);
-                        c3.showDetails(choix_db, 3);
+                        Interface.Motorcycle m4 = new Interface.Motorcycle("UJ898", "HAUJIN", "Red", "4537788egte");
+                        Interface.Cars c4 = new Interface.Cars("fgt34", "WOLVSWAGGEN", "Metal", "oo9ieiie");
+                        Interface.Motorcycle m5 = new Interface.Motorcycle("UJ898", "HAUJIN", "Red", "4537788egte");
+                        Interface.Cars c5 = new Interface.Cars("fgt34", "WOLVSWAGGEN", "Metal", "oo9ieiie");
+                        Interface.Motorcycle m6 = new Interface.Motorcycle("UJ898", "HAUJIN", "Red", "4537788egte");
+                        Interface.Cars c6 = new Interface.Cars("fgt34", "WOLVSWAGGEN", "Metal", "oo9ieiie");
+                        m4.Add(choix_db);
+                        c4.Add(choix_db);
+                        m5.Add(choix_db);
+                        c5.Add(choix_db);
+                        m6.Add(choix_db);
+                        c6.Add(choix_db);
+                        m4.showDetails(choix_db, 4);
+                        c4.showDetails(choix_db, 4);
+                        m5.showDetails(choix_db, 5);
+                        c5.showDetails(choix_db, 5);
+                        m6.showDetails(choix_db, 6);
+                        c6.showDetails(choix_db, 6);
                     }
                 }
                 
